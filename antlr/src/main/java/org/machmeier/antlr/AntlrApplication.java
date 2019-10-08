1269f0f2762af3ae425b07e887abbf0d113b0f19
Stefan Machmeier <stefan-machmeier@outlook.com>
2019-10-08 14:27:16 +0200

    Initial project

---
+++ antlr/src/main/java/org/machmeier/antlr/AntlrApplication.java
@@ -0,0 +1,13 @@
+package org.machmeier.antlr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntlrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntlrApplication.class, args);
+	}
+
+}
